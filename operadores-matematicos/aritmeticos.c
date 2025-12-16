da71c6a439717725c09bb12fbdfbd192f5d4a08c
Renato Brito <dev.renatobrito@gmail.com>
2025-12-16 00:23:28 -0300

    :adhesive_bandage: renomea arquivos

---
+++ operadores-matematicos/aritmeticos.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+  /*
+    Soma: +
+    Subtração: -
+    Multiplicação: *
+    Divisão: /
+  */
+
+  int numero1, numero2;
+  int soma, subtracao, multiplicacao, divisao;
+
+  printf("------------------------------\n");
+  printf("--- OPERADORES ARITMÉTICOS ---\n");
+  printf("------------------------------\n");
+  
+  printf("Entre com o primeiro número: ");
+  scanf("%d", &numero1);
+  printf("Entre com o segundo número: ");
+  scanf("%d", &numero2);
+  printf("------------------------------\n");
+  
+  soma = numero1 + numero2;
+  subtracao = numero1 - numero2;
+  multiplicacao = numero1 * numero2;
+  divisao = numero1 / numero2;
+
+  printf("A soma é: %d\n", soma);
+  printf("A subtracao é: %d\n", subtracao);
+  printf("A multiplicacao é: %d\n", multiplicacao);
+  printf("A divisao é: %d\n", divisao);
+  printf("------------------------------\n");
+
+  return 0;
+}
---
+++ operadores-matematicos/atribuicao.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+  /*
+    Atribuição simples (=)
+    Atribuição com soma (+=)
+    Atribuição com subtração (-=)
+    Atribuição com multiplicação (*=)
+    Atribuição com divisão (/=)
+  */
+
+  int numero = 10, resultado;
+
+  printf("----------------------------------\n");
+  printf("---- OPERADORES DE ATRIBUIÇÃO ----\n");
+  printf("----------------------------------\n");
+
+  resultado = 10;
+  printf("Resultado: %d\n", resultado);
+
+  resultado += 15;
+  printf("Novo resultado com soma:  %d\n", resultado);
+
+  resultado -= numero;
+  printf("Novo resultado com subtração: %d\n", resultado);
+
+  resultado *= 3;
+  printf("Novo resultado com multiplicação: %d\n", resultado);
+
+  resultado /= 2;
+  printf("Novo resultado com divisão: %d\n", resultado);
+  printf("-------------------------\n");
+
+  return 0;
+}
