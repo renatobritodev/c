092b9a2950a055a497ce441bbedb5631ccb3e593
Renato Brito <dev.renatobrito@gmail.com>
2025-12-18 20:26:58 -0300

    :sparkles: feat: declaração de variáveis + funções de entrada e saída de dados

--- media-notas/media-notas.c
+++ media-notas/media-notas.c
@@ -3,33 +3,33 @@
 int main()
 {
   int idade, matricula;
-  float nota1, nota2, nota3, soma_nota, media;
+  float nota1, nota2, nota3, somador_notas, media;
   char nome[50];
 
-  printf("----------------------\n");
-  printf("--- MÉDIA DE NOTAS ---\n");
-  printf("----------------------\n");
+  printf("-----------------------\n");
+  printf("--- MÉDIA DAS NOTAS ---\n");
+  printf("-----------------------\n");
 
   printf("Nome: ");
   scanf("%s", nome);
-  
+
   printf("Idade: ");
   scanf("%d", &idade);
-  
+
   printf("Matrícula: ");
   scanf("%d", &matricula);
 
-  printf("Nota 1: ");
+  printf("Digite a sua primeira nota: ");
   scanf("%f", &nota1);
 
-  printf("Nota 2: ");
+  printf("Digite a sua segunda nota: ");
   scanf("%f", &nota2);
 
-  printf("Nota 3: ");
+  printf("Digite a sua terceira nota: ");
   scanf("%f", &nota3);
 
-  soma_nota = nota1 + nota2 + nota3;
-  media = soma_nota / 3;
+  somador_notas = nota1 + nota2 + nota3;
+  media = somador_notas / 3;
 
   printf("Média do aluno %s é: %.1fpts.\n", nome, media);
 
---
+++ operadores-matematicos/conversao-tipos-dados.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 10;
+  int b = 3;
+  float quociente = a + b;
+
+  printf("A soma de a e b é: %.2f", quociente);
+
+  return 0;
+}
