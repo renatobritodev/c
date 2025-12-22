9ef4a837944bdd31615538a3d3192349d1029330
Renato Brito <dev.renatobrito@gmail.com>
2025-12-22 19:56:50 -0300

    :sparkles: feat: cria - seguno desafio - gerenciamento de estoque

---
+++ gerenciamento-estoque/index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  char produtoA[30] = "Produto A";
+  char produtoB[30] = "Produto B";
+
+  unsigned int estoqueA = 1000;
+  unsigned int estoqueB = 2000;
+
+  float valorA = 10.50;
+  float valorB = 20.40;
+
+  unsigned int estoqueMinimoA = 500;
+  unsigned int estoqueMinimoB = 2500;
+
+  double valorTotalA;
+  double valorTotalA;
+
+  return 0;
+}
